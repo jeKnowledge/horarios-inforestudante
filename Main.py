3289403ec119c250962c8a1e10fa3719b9c58f0e
Mike EVMM <mikemakesgames@gmail.com>
2016-03-06 13:34:24 +0000

    .Created TImetableMaker.py .Added classes to mock user data

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ import ClassFilter
 
 print("started")
 
-# Mock schedule data
+# Mock user data
 semester = 1
-classesId = [178771]
+classesId = [178771, 178867, 181136]
 
 # Parse csv into array of stripped data
 dataRawArray = DataParser.csvIntoRawArray(DataParser.openFile("HorariosRaw.csv"))
@@ -17,3 +17,5 @@ for disciplina in classesId:
     possibleClasses = ClassFilter.possibleClasses(dataStrippedArray, semester, disciplina) #All classes for the semester
     noDuplicates = ClassFilter.removeDuplicates(possibleClasses, True)
     aulas[disciplina] = ClassFilter.groupByType(noDuplicates)
+
+#Make timetable
---
+++ TimetableMaker.py
@@ -0,0 +1,16 @@
+#Takes in dictionary of structure:
+# { CLASS_ID:{
+#       T:{
+#           CLASS_T1,
+#           CLASS_T...
+#       },
+#       TP:{
+#           CLASS_TP...
+#       },
+#       ...
+#   }, ...
+# }
+
+# Returns array of valid class combinations
+def dictToValidTimetables(dictionary):
+    print("TODO")
